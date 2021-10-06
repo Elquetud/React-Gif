2cdfd167d6d10c3f344dd2af7f2298c464285d72
Mona Lisa <jesusromeroarmenta@outlook.com>
2021-10-06 02:27:10 -0600

    cambio

--- src/helpers/GetGift.js
+++ src/helpers/GetGift.js
@@ -1,6 +1,6 @@
 
 export const GetGift = async(category)=>{ //funcion de async 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=TzAZbsVYxhqPt89UI9sHOy530jLw3JjV&q=${encodeURI(category)}&limit=15`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=TzAZbsVYxhqPt89UI9sHOy530jLw3JjV&q=${encodeURI(category)}&limit=20`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
